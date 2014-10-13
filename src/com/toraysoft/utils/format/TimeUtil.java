715e323c91d106354a752e9e8b4e4975f632616a
tanq <tanq@toraysoft.com>
2014-10-13 23:07:19 +0800

    update time util

--- src/com/toraysoft/utils/format/TimeUtil.java
+++ src/com/toraysoft/utils/format/TimeUtil.java
@@ -1,5 +1,12 @@
 package com.toraysoft.utils.format;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.TimeZone;
+
+import android.annotation.SuppressLint;
+
 public class TimeUtil {
 	public static final String[] starArr = { "魔羯座", "水瓶座", "双鱼座", "牡羊座", "金牛座",
 			"双子座", "巨蟹座", "狮子座", "处女座", "天秤座", "天蝎座", "射手座" };
@@ -52,5 +59,76 @@ public class TimeUtil {
 		if (constellation != null)
 			sb.append(constellation);
 		return sb.toString();
+	}
+	
+	
+	public static String getConverTime(String time,boolean isDetail){
+		  String time1=time.replaceAll("T", "-");
+		  String time2=time1.replaceAll("Z", "");
+
+		  SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd-hh:mm:ss");
+	      sdf.setTimeZone(TimeZone.getTimeZone("GMT"));
+		  try {
+			long millionSeconds = sdf.parse(time2).getTime();
+			if(isDetail)
+			return converDetailTime(millionSeconds);
+			else
+			return converTime(millionSeconds);
+		} catch (ParseException e) {
+			e.printStackTrace();
+		}
+		return "";	
+	}
+	
+	public static String converTime(long timestamp) {
+		long currentSeconds = System.currentTimeMillis() / 1000;
+		long timeGap = currentSeconds - timestamp / 1000;// 与现在时间相差秒数
+		String timeStr = null;
+		if (timeGap > 7* 24 * 60 *60){
+			timeStr = getDateTimeByMillisecond(timestamp);//几月几号
+		} else if (timeGap > 24 * 60 * 60) {// 1天以上
+			timeStr = timeGap / (24 * 60 * 60) + "天前";
+		} else if (timeGap > 60 * 60) {// 1小时-24小时
+			timeStr = timeGap / (60 * 60) + "小时前";
+		} else if (timeGap > 60) {// 1分钟-59分钟
+			timeStr = timeGap / 60 + "分钟前";
+		} else {// 1秒钟-59秒钟
+			timeStr = timeGap + "秒前";
+		}
+		return timeStr;
+	}
+	
+	//几月几号
+	@SuppressLint("SimpleDateFormat")
+	public static String getDateTimeByMillisecond(long str) {  
+	    Date date = new Date(Long.valueOf(str));  
+	    SimpleDateFormat format = new SimpleDateFormat("MM-dd");  
+	    String time = format.format(date); 
+	    return time;  
+	}
+	
+	public static String converDetailTime(long timestamp) {
+		long currentSeconds = System.currentTimeMillis() / 1000;
+		long timeGap = currentSeconds - timestamp / 1000;// 与现在时间相差秒数
+		String timeStr = null;
+		if (timeGap > 24 * 60 * 60){
+			timeStr = getDetailDateTimeByMillisecond(timestamp);//几月几号 几时几分
+		} else if (timeGap > 60 * 60) {// 1小时-24小时
+			timeStr = timeGap / (60 * 60) + "小时前";
+		} else if (timeGap > 60) {// 1分钟-59分钟
+			timeStr = timeGap / 60 + "分钟前";
+		} else {// 1秒钟-59秒钟
+			timeStr = timeGap + "秒前";
+		}
+		return timeStr;
+	}
+	
+	//几月几号
+	@SuppressLint("SimpleDateFormat")
+	public static String getDetailDateTimeByMillisecond(long str) {  
+	    Date date = new Date(Long.valueOf(str));  
+	    SimpleDateFormat format = new SimpleDateFormat("MM月dd日 HH:mm");  
+	    String time = format.format(date); 
+	    return time;  
 	}
 }
