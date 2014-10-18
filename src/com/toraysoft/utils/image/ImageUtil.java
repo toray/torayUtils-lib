5ea3abb2996f39377827a0f8dbd08cf196874492
tanq <tanq@toraysoft.com>
2014-10-18 15:33:26 +0800

    ImageLoader

--- src/com/toraysoft/utils/image/ImageUtil.java
+++ src/com/toraysoft/utils/image/ImageUtil.java
@@ -6,6 +6,7 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.sql.NClob;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -44,6 +45,7 @@ import com.android.volley.toolbox.ImageLoader;
 import com.android.volley.toolbox.ImageLoader.ImageCache;
 import com.android.volley.toolbox.ImageLoader.ImageContainer;
 import com.android.volley.toolbox.ImageLoader.ImageListener;
+import com.android.volley.toolbox.Volley;
 import com.toraysoft.utils.cache.CacheUtil;
 
 public class ImageUtil {
@@ -73,31 +75,35 @@ public class ImageUtil {
 	}
 
 	public ImageLoader getImageLoader() {
+//		if (mImageLoader == null) {
+//
+//			File cacheDir = new File(mContext.getCacheDir(), "temp");
+//
+//			HttpStack stack = null;
+//			if (Build.VERSION.SDK_INT >= 9) {
+//				stack = new HurlStack();
+//			} else {
+//				// Prior to Gingerbread, HttpUrlConnection was unreliable.
+//				// See:
+//				// http://android-developers.blogspot.com/2011/09/androids-http-clients.html
+//				stack = new HttpClientStack(
+//						AndroidHttpClient.newInstance("TorayImage"));
+//			}
+//
+//			Network network = new BasicNetwork(stack);
+//			RequestQueue queue = new RequestQueue(new DiskBasedCache(cacheDir),
+//					network, 5);
+//			queue.start();
+//
+//			// mImageLoader = new ImageLoader(Volley.newRequestQueue(context
+//			// .getApplicationContext()), mBitmapLruCache);
+//
+//			mImageLoader = new ImageLoader(queue, mBitmapLruCache);
+//
+//		}
+//		
 		if (mImageLoader == null) {
-
-			File cacheDir = new File(mContext.getCacheDir(), "temp");
-
-			HttpStack stack = null;
-			if (Build.VERSION.SDK_INT >= 9) {
-				stack = new HurlStack();
-			} else {
-				// Prior to Gingerbread, HttpUrlConnection was unreliable.
-				// See:
-				// http://android-developers.blogspot.com/2011/09/androids-http-clients.html
-				stack = new HttpClientStack(
-						AndroidHttpClient.newInstance("TorayImage"));
-			}
-
-			Network network = new BasicNetwork(stack);
-			RequestQueue queue = new RequestQueue(new DiskBasedCache(cacheDir),
-					network, 5);
-			queue.start();
-
-			// mImageLoader = new ImageLoader(Volley.newRequestQueue(context
-			// .getApplicationContext()), mBitmapLruCache);
-
-			mImageLoader = new ImageLoader(queue, mBitmapLruCache);
-
+			mImageLoader = new ImageLoader(Volley.newRequestQueue(mContext), mBitmapLruCache);
 		}
 		return mImageLoader;
 	}
