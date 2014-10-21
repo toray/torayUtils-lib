fdfe7e9f73b564fb85a3d64ce79f2865ae30778d
jclang <liang@toraysoft.com>
2014-10-21 18:19:23 +0800

    fix time bug

--- src/com/toraysoft/utils/format/TimeUtil.java
+++ src/com/toraysoft/utils/format/TimeUtil.java
@@ -134,6 +134,26 @@ public class TimeUtil {
 		return timeStr;
 	}
 	
+	public static String converTimeThird(long timestamp) {
+		long currentSeconds = System.currentTimeMillis() / 1000;
+		long timeGap = currentSeconds - timestamp;// 与现在时间相差秒数
+		String timeStr = null;
+		if (timeGap > 7* 24 * 60 *60){
+			timeStr = getDateTimeByMillisecond(timestamp);//几月几号
+		} else if (timeGap > 24 * 60 * 60) {// 1天以上
+			timeStr = timeGap / (24 * 60 * 60) + "天前";
+		} else if (timeGap > 60 * 60) {// 1小时-24小时
+			timeStr = timeGap / (60 * 60) + "小时前";
+		} else if (timeGap > 60) {// 1分钟-59分钟
+			timeStr = timeGap / 60 + "分钟前";
+		} else if(timeGap > 0){// 1秒钟-59秒钟
+			timeStr = timeGap + "秒前";
+		} else {
+			timeStr = "刚刚";
+		}
+		return timeStr;
+	}
+	
 	//几月几号
 	@SuppressLint("SimpleDateFormat")
 	public static String getDateTimeByMillisecond(long str) {  
