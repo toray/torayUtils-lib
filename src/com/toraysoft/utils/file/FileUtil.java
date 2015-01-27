53c7b168a1d17ae220325bec910eecd48618c282
jclang <liang@toraysoft.com>
2015-01-27 15:53:18 +0800

    fix fileutil

--- src/com/toraysoft/utils/file/FileUtil.java
+++ src/com/toraysoft/utils/file/FileUtil.java
@@ -2,12 +2,18 @@ package com.toraysoft.utils.file;
 
 import java.io.BufferedInputStream;
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.RandomAccessFile;
 import java.util.ArrayList;
 import java.util.List;
 
 import android.content.Context;
+import android.util.Log;
 
 public class FileUtil {
 
@@ -29,5 +35,25 @@ public class FileUtil {
 			ret.add(line);
 		}
 		return ret;
+	}
+
+	// 将字符串写入到文本文件中
+	public static void writeTxtFile(String strcontent, String strFilePath) {
+		// 每次写入时，都换行写
+		String strContent = strcontent + "\n";
+		try {
+			File file = new File(strFilePath);
+			if (!file.exists()) {
+				Log.d("File", "Create the file:" + strFilePath);
+				file.createNewFile();
+			}
+			RandomAccessFile raf = new RandomAccessFile(file, "rw");
+			raf.seek(file.length());
+			raf.write(strContent.getBytes());
+			raf.close();
+		} catch (Exception e) {
+			Log.e("File", "Error on write File.");
+			e.printStackTrace();
+		}
 	}
 }
