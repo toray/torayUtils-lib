7ade8125d352102b3d42fbc411aa60d2ba95bc28
pengdy <13610337335@163.com>
2015-08-06 18:08:56 +0800

    Changes to be committed: 	modified:   project.properties 	modified:   src/com/toraysoft/manager/AppManager.java

--- src/com/toraysoft/manager/AppManager.java
+++ src/com/toraysoft/manager/AppManager.java
@@ -1,5 +1,10 @@
 package com.toraysoft.manager;
 
+import java.io.DataOutputStream;
+import java.io.File;
+import java.io.IOException;
+import java.io.OutputStream;
+
 import android.annotation.SuppressLint;
 import android.content.ClipData;
 import android.content.ClipboardManager;
@@ -125,5 +130,49 @@ public class AppManager {
 			ClipData clip = ClipData.newPlainText(content,content);
 			clipboardManager.setPrimaryClip(clip);
 		}
+	}
+	
+	/**
+	 * 静默安装
+	 * @param file
+	 * @return
+	 */
+	public boolean slientInstall(File file) {
+		boolean result = false;
+
+		if (file != null && file.exists()) {
+			Process process = null;
+			OutputStream out = null;
+			try {
+				process = Runtime.getRuntime().exec("su");
+				out = process.getOutputStream();
+				DataOutputStream dataOutputStream = new DataOutputStream(out);
+				dataOutputStream.writeBytes("chmod 777 " + file.getPath()
+						+ "\n");
+				dataOutputStream
+						.writeBytes("LD_LIBRARY_PATH=/vendor/lib:/system/lib pm install -r "
+								+ file.getPath());
+				// 提交命令
+				dataOutputStream.flush();
+				// 关闭流操作
+				dataOutputStream.close();
+				out.close();
+				int value = process.waitFor();
+
+				// 代表成功
+				if (value == 0) {
+					result = true;
+				} else if (value == 1) { // 失败
+					result = false;
+				} else { // 未知情况
+					result = false;
+				}
+			} catch (IOException e) {
+				e.printStackTrace();
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+		return result;
 	}
 }
